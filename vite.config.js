96f00b01a00631d4623b85a9a9fd44c8f57858a5
Portable <clementanthony@hotmail.fr>
2024-05-31 13:03:35 +0200

    commit : Push yml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'ACtech', // Remplacez <nom-du-dépôt> par le nom de votre dépôt GitHub
 })
