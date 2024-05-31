3da5029efe8723c74382e455c68739656f0be85f
Portable <clementanthony@hotmail.fr>
2024-05-31 13:05:09 +0200

    commit : Add Files yml

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'ACtech', // Remplacez <nom-du-dépôt> par le nom de votre dépôt GitHub
+  base: '/ACtech/'
 })
