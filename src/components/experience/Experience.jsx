82548b03420497f0aabdd5af762516a5fd3ca6c1
Portable <clementanthony@hotmail.fr>
2024-06-03 18:27:45 +0200

    Commit : front modification experience about

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -28,45 +28,62 @@ export default function Experience() {
     <>
       <Section>
         <div className="experienceMain">
-          <p>EXPERIENCE</p>
-          <img className="xpImg" src={sale} alt="" srcset="" />
+          <p className="accomodationP">EXPERIENCE</p>
+          <p className="accomodationPChildren">
+            Un cours résumé des différents projets que j'ai pu réaliser. En tant
+            que
+            <b> développeur Fullstack</b> , j'ai réalisé une variété de projets
+            qui démontrent mes compétences en <b>JavaScript</b> , tant pour le{" "}
+            <b>frontend </b> que pour le <b>backend</b> . J'ai conçu et
+            développé des{" "}
+            <b>
+              interfaces web dynamiques et des applications web progressives
+              (PWA)
+            </b>{" "}
+            qui offrent une expérience utilisateur <b>fluide et interactive.</b>{" "}
+            En parallèle, j'ai mis en place des backends{" "}
+            <b>robustes et performants</b> , garantissant la{" "}
+            <b>fiabilité et l'efficacité des systèmes</b> . Mon expertise en
+            JavaScript me permet de créer des solutions complètes, de la
+            conception d'interfaces utilisateur attrayantes à la gestion de la
+            logique serveur.
+          </p>
+          {/* <img className="xpImg" src={sale} alt="" srcset="" /> */}
         </div>
         <div className="experienceMainDescription">
           <Section>
             <ul>
               <li>
                 <p>
-                  Avec 5 ans d'expérience professionnelle et <b>26 ans de passion </b> ,
-                  le support technique n'a plus de secret pour moi.{" "}
-                </p>
-                <p>
-                  Ces dernières années, j'ai acquis de solides compétences en
-                  <b> développement de sites web et de web applications.</b>   J'ai
-                  également participé à des <b>projets fullstack</b>  en entreprise pour
-                  le développement interne.
+                  Avec 5 ans d'expérience professionnelle et{" "}
+                  <b>26 ans de passion </b> , le support technique n'a plus de
+                  secret pour moi. Ces dernières années, j'ai acquis de solides
+                  compétences en
+                  <b>
+                    {" "}
+                    développement de sites web et de web applications.
+                  </b>{" "}
+                  J'ai également participé à des <b>projets fullstack</b> en
+                  entreprise pour le développement interne.{" "}
                 </p>
               </li>
               <li>
                 <p>
-                <b>La gestion </b>complète de boutique de vêtement m'a fait aquérir
-                  des compétence clefs !
-                </p>
-                <p>
-                  Notemment la<b> gestion des équipes, des produits</b>  mais surtout la 
-                  <b> clientèle et fidélisation.</b>  Plusieurs <b>grandes enseignes</b>  m'ont
-                  fait confiance !
+                  <b>La gestion </b>complète de boutique de vêtement m'a fait
+                  aquérir des compétence clefs ! Notemment la
+                  <b> gestion des équipes, des produits</b> mais surtout la
+                  <b> clientèle et fidélisation.</b> Plusieurs{" "}
+                  <b>grandes enseignes</b> m'ont fait confiance !
                 </p>
               </li>
 
               <li>
                 <p>
                   {" "}
-                  Engagée volontaire de l'armée de terre durant 5 ans au <b>3èmes régiment du génie</b> 
-                  
-                </p>
-                <p>
-                <b>Sapeur de combat du génie</b> mais également en <b> appui</b> des sections de
-                  combat.
+                  Engagée volontaire de l'armée de terre durant 5 ans au{" "}
+                  <b>3èmes régiment du génie</b>
+                  <b>Sapeur de combat du génie</b> mais également en{" "}
+                  <b> appui</b> des sections de combat.
                 </p>
               </li>
             </ul>
